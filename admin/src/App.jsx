cf47ffe3d5674672aff3e7878b539debc5a15cd9
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-07-30 20:24:55 +0100

    Admin Sidebr Implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import SignupPage from './pages/SignupPage'
 import DashboardPage from './pages/DashboardPage'
+import AdminSidebar from './components/AdminSidebar'
 
 const App = () => {
   return (
     <>
       {/* <SignupPage /> */}
 
-      <DashboardPage />
+      {/* <DashboardPage /> */}
+
+      <AdminSidebar />
     </>
   )
 }
